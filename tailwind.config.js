f9bc2b881f4a5dbedd996da4abe21cf256fe2c48
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-10-01 23:03:51 -0300

    add color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
 
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      background: colors.slate[900],
+      primary: "#02ee25",
+    },
+    fontFamily: {
+      "fira-code": ["Fira Code"],
+    },
     extend: {},
   },
   plugins: [],
