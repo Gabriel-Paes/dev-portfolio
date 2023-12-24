e9ddae36894774338cfdf1d181dc3c4b9cfa577c
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-12-24 12:03:27 -0300

    Add scroll style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,23 @@ import colors from "tailwindcss/colors";
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      background: colors.slate[900],
-      primary: "#02ee25",
-    },
     fontFamily: {
       "fira-code": ["Fira Code"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        background: colors.slate[900],
+        primary: "#02ee25",
+      },
+      backgroundImage: {
+        "gradient-blue-pink": "linear-gradient(to right, #3490dc, #f6993f)",
+        "gradient-blue-green":
+          "linear-gradient(40deg, rgba(15,23,42,1) 15%, rgba(24,9,121,1) 66%, rgba(2,238,37,1) 100%)",
+        "gradient-purple-green": "linear-gradient(to left, #805ad5, #4a90e2)",
+      },
+    },
   },
   plugins: [],
 };
